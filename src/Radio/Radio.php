7f68f7b8475b40a57a079c8cd337666ed5e742fc
crewstyle <ach@takeatea.com>
2016-04-29 15:14:27 +0200

    first commit

---
+++ src/Radio/Radio.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace GetOlympus\Field;
+
+use GetOlympus\Hera\Controllers\Field;
+use GetOlympus\Hera\Controllers\Translate;
+
+/**
+ * Builds Radio field.
+ *
+ * @package Field
+ * @subpackage Radio
+ * @author Achraf Chouk <achrafchouk@gmail.com>
+ * @since 0.0.1
+ *
+ * @see https://olympus.readme.io/v1.0/docs/radio-field
+ *
+ */
+
+class Radio extends Field
+{
+    /**
+     * @var string
+     */
+    protected $faIcon = 'fa-dot-circle-o';
+
+    /**
+     * @var string
+     */
+    protected $template = 'radio.html.twig';
+
+    /**
+     * Prepare HTML component.
+     *
+     * @param array $content
+     * @param array $details
+     *
+     * @since 0.0.1
+     */
+    protected function getVars($content, $details = [])
+    {
+        // Build defaults
+        $defaults = [
+            'id' => '',
+            'title' => Translate::t('radio.title'),
+            'default' => '',
+            'description' => '',
+            'mode' => '',
+            'options' => [],
+
+            // details
+            'post' => 0,
+            'prefix' => '',
+            'template' => 'pages',
+
+            // texts
+            't_no_options' => Translate::t('radio.no_options'),
+        ];
+
+        // Build defaults data
+        $vars = array_merge($defaults, $content);
+
+        // Retrieve field value
+        $vars['val'] = $this->getValue($details, $vars['default'], $content['id'], true);
+
+        // Update vars
+        $this->getField()->setVars($vars);
+    }
+}
